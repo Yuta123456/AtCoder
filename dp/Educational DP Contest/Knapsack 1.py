5bfc19749aa3a1e6e5ec96991819aa5c05eebba8
Yuta123456 <yuuta09090530@icloud.com>
2019-11-21 18:42:52 +0900

    20191121

--- dp/Educational DP Contest/Knapsack 1.py
+++ dp/Educational DP Contest/Knapsack 1.py
@@ -1,15 +1,13 @@
 n, W = list(map(int, input().split()))
-w = [0 for i in range(n)]
-v = [0 for i in range(n)]
+w = [0 for i in range(n + 1)]
+v = [0 for i in range(n + 1)]
 for i in range(n):
-    w[i], v[i] = list(map(int, input().split()))
-dp = [[0 for i in range(W + 1)] for j in range(n)]
-for j in range(W+1):
-    for i in range(n):
-        p = j - w[i]
-        s = dp[i - 1][j]
-        if p >= 0:
-            dp[i][j] = max(s, dp[i - 1][p] + v[i])
+    w[i + 1], v[i + 1] = list(map(int, input().split()))
+dp = [[0 for i in range(W+1)] for j in range(n+1)]
+for i in range(1,n+1):
+    for j in range(1,W+1):
+        if w[i] > j:
+            dp[i][j] = dp[i - 1][j]
         else:
-            dp[i][j] = s
-print(dp[-1][-1])
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i])
+print(int(dp[n][W]))
--- dp/Educational DP Contest/LCS.py
+++ dp/Educational DP Contest/LCS.py
@@ -1,20 +1,17 @@
-import sys
-input = sys.stdin.readline
-
 s = input()
 t = input()
 
 #dp[i][j] => s[i], t[j]までの最長部分文字列
 
-
 dp = [[""] * (len(t) + 1) for j in range(len(s) + 1)]
-for j in range(len(t)):
-    for i in range(len(s)):
-        if s[i] == t[j]:
-            v =  [dp[i][j] + s[i], dp[i + 1][j],dp[i][j + 1]]
-            v = sorted(v, key = lambda x:len(x))
+for i in range(1,len(s)+1):
+    for j in range(1,len(t)+1):
+        if s[i - 1] == t[j - 1]:
+            dp[i][j] = "".join(dp[i - 1][j] + s[i - 1])
         else:
-            v = [dp[i + 1][j], dp[i][j + 1]]
-            v = sorted(v, key = lambda x:len(x))
-        dp[i + 1][j + 1] = v[-1]
+            #上か左の長いほうを入れる
+            if len(dp[i][j-1]) < len(dp[i-1][j]):
+                dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = dp[i][j - 1]
 print(dp[-1][-1])
